fa33307677401313624d08f982f33afbbfea9505
Austin Papritz <austin@papritz.dev>
2023-04-07 15:15:12 -0700

    added Physics to scene

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -12,20 +12,22 @@ export default function Experience() {
       <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
       <ambientLight intensity={0.5} />
 
-      <mesh castShadow position={[-2, 2, 0]}>
-        <sphereGeometry />
-        <meshStandardMaterial color="orange" />
-      </mesh>
+      <Physics>
+        <mesh castShadow position={[-2, 2, 0]}>
+          <sphereGeometry />
+          <meshStandardMaterial color="orange" />
+        </mesh>
 
-      <mesh castShadow position={[2, 2, 0]}>
-        <boxGeometry />
-        <meshStandardMaterial color="mediumpurple" />
-      </mesh>
+        <mesh castShadow position={[2, 2, 0]}>
+          <boxGeometry />
+          <meshStandardMaterial color="mediumpurple" />
+        </mesh>
 
-      <mesh receiveShadow position-y={-1.25}>
-        <boxGeometry args={[10, 0.5, 10]} />
-        <meshStandardMaterial color="greenyellow" />
-      </mesh>
+        <mesh receiveShadow position-y={-1.25}>
+          <boxGeometry args={[10, 0.5, 10]} />
+          <meshStandardMaterial color="greenyellow" />
+        </mesh>
+      </Physics>
     </>
   )
 }
